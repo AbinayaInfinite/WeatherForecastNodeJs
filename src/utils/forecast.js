d8006945270c52804bd08705a7b30a7b73fbf356
abinayasekararunkumar <abi.cse13@gmail.com>
2022-06-20 08:17:05 +0400

    Added additional info on forecast page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast= (data,callback)=>{
         }else if (body.error){
             callback("Unable to find location",undefined)
         }else{
-            callback(undefined,body.current.weather_descriptions[0]+". The current temperature is " + body.current.temperature + ". It feels like " + body.current.feelslike + "!")
+            callback(undefined,body.current.weather_descriptions[0]+". The current temperature is " + body.current.temperature + ". It feels like " + body.current.feelslike + "!"+" The current Humidity is " + body.current.humidity +"!")
         }
     })
 }
